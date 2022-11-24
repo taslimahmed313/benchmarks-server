79ac50c49a99f79c2aad9a48d7b8198581c36456
Taslim <taslimahmed313@gmil.com>
2022-11-24 17:05:56 +0600

    Post All Booking Data

--- index.js
+++ index.js
@@ -26,12 +26,38 @@ async function run(){
         const categoriesCollection = client
           .db("resellWizards")
           .collection("categories");
+
+        // All Category Based  Books Are Stored Here------------------->>>>  
+        const booksCollection = client.db("resellWizards").collection("books");
+
+        // All Bookings Are Stored Here------------->>>>>
+        const bookingsCollection = client.db("resellWizards").collection("bookings");
         
+
+
         // Get All Categories and Show on Home Page------------------->>>>>>>>>>>>>>>>
         app.get("/categories", async(req, res)=>{
             const query = {};
             const result = await categoriesCollection.find(query).toArray();
             res.send(result);
+        })
+
+
+        // Get All Books With Params Id and Show Category:id routes----------->>>>>>>>>>>>
+        app.get("/category/:id", async(req, res)=>{
+            const id = req.params.id;
+            const query = {};
+            const allBooks = await booksCollection.find(query).toArray();
+            const categoryBooks = allBooks.filter(n => n.category_id === id);
+            res.send(categoryBooks);
+        })
+
+
+        // Posted All Bookings from Booking Modal------------------->>>>>
+        app.post("/bookings", async(req, res)=>{
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result); 
         })
 
     }
