24d51a26377c4c4e3d1a0b7eab357e09dd1eef86
Taslim <taslimahmed313@gmil.com>
2022-11-27 10:40:18 +0600

    Report Items Delete operation

--- index.js
+++ index.js
@@ -221,6 +221,14 @@ async function run(){
         res.send(result);
       })
 
+      // Delete Reported Product--------------->>>>>>>>>>>>>>>>>>>>>>
+      app.delete("/reports/:id", async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await booksCollection.deleteOne(query);
+        res.send(result);
+      })
+
 
 
 
