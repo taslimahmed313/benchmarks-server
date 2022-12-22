3d5c67b554dfe84b15068733a7af04d68b46eea4
Taslim <taslimahmed313@gmil.com>
2022-12-22 09:16:48 +0600

    simple

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ async function run(){
         res.send(result);
       });
 
-      // Delete Reported Product--------------->>>>>>>>>>>>>>>>>>>>>>
+      // Delete Reported Product--------------->>>>>>>>>>>>>>>>>>>>
       app.delete("/reports/:id", async (req, res) => {
         const id = req.params.id;
         const query = { _id: ObjectId(id) };
